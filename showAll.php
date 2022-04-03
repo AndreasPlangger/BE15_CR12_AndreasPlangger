fae3947317503b7dc4fddafb7789776a04f3cc75
AndreasPlangger <97440842+AndreasPlangger@users.noreply.github.com>
2022-04-04 00:36:19 +0200

    Add files via upload

---
+++ showAll.php
@@ -0,0 +1,3 @@
+<!-- Bonus points:
+
+(20) after you create your own API (displayAll.php), create a new file called showAll.php, and that file will have a button, when you click on the button, you will show all data that you got from the API using AJAX. -->
