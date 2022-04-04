eb734f4d80abd2cef7111df95fd81020fd4d5023
AndreasPlangger <andreas_plangger@hotmail.com>
2022-04-04 22:34:54 +0200

    delete and new

--- actions/a_convertJSON.php
+++ actions/a_convertJSON.php
@@ -1,10 +1,11 @@
-<?php 
+<?php
 //This will convert to a JSON Output
-function response($status, $status_message, $data) {
+function response($status, $status_message, $data)
+{
     $response["status"] = $status;
     $response["status_message"] = $status_message;
-    $response["data"] = $data;
+    $all_travel_offers["data"] = $data;
 
     //output JSON
-    echo json_encode($response);
+    echo json_encode($all_travel_offers);
 }
