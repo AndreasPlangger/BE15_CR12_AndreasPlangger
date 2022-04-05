764f8ff54efb531e034dcc719e219abebe5023dd
AndreasPlangger <andreas_plangger@hotmail.com>
2022-04-05 21:44:01 +0200

    delete

--- allTravelOffers.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once 'actions/db_connect.php';
-
-$sql = "SELECT * FROM travel_offers";
-$result = mysqli_query($connect, $sql);
-$all_travel_offers = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
-echo json_encode($all_travel_offers);
